feb8c57a2230312062a8f95eb208cce64def826f
alphajuliet <andrew@alphajuliet.com>
2023-12-14 10:36:52 +1100

    Solve 11.2

--- src/aoc23/day11.clj
+++ src/aoc23/day11.clj
@@ -31,17 +31,6 @@
         cc (zero-rows (util/T mat))]
    [rr cc]))
 
-(defn insert-rows
-  "Insert one zero row after every given row number"
-  [row-numbers mat]
-  (let [cols (count (first mat))]
-    (reduce (fn [m r]
-              (concat (take (inc r) m)
-                      (vector (repeat cols 0))
-                      (take-last (- (count m) (inc r)) m)))
-            mat
-            (reverse row-numbers))))
-
 (defn find-ones
   "Find the coordinates of all the ones in the matrix"
   [mat]
@@ -50,23 +39,46 @@
         :when (= 1 (get-in mat [r c]))]
     [r c]))
 
+(defn crossings
+  "Determine how many of the points lie inside the range"
+  [start end points]
+  (if (<= start end)
+    (count (filter #(< start % end) points))
+    (count (filter #(> start % end) points))))
+
+(defn expanded-manhattan
+  "Return the manhattan distance between p1 and p2, plus an increment for every time the shortest path
+   crosses one of the zero rows or columns."
+  [[r1 c1] [r2 c2] [zrows zcols] incr]
+  (let [dist (util/manhattan [r1 c1] [r2 c2])
+        dr (* incr (crossings r1 r2 zrows))
+        dc (* incr (crossings c1 c2 zcols))]
+    (+ dist dr dc)))
+
 (defn distances
   "Find the distances between all the coordinates in the list"
-  [points]
-  (for [[p1 p2] (combo/combinations points 2)]
-    (util/manhattan p1 p2)))
+  [insertions incr coords]
+;;   (set! *print-length* 5) ; help the debugger
+  (for [[p1 p2] (combo/combinations coords 2)]
+    (expanded-manhattan p1 p2 insertions incr)))
 
 (defn part1
   [f]
   (let [data (read-data f)
         insertions (find-zero-rows-cols data)]
     (->> data
-        (insert-rows (first insertions))
-        util/T
-        (insert-rows (second insertions))
-        util/T
         find-ones
-        distances
+        (distances insertions 1)
         (apply +))))
+
+(defn part2
+  "If incr > 1, then enter incr-1"
+  [f incr]
+  (let [data (read-data f)
+        insertions (find-zero-rows-cols data)]
+    (->> data
+         find-ones
+         (distances insertions incr)
+         (apply +))))
 
 ;; The End
