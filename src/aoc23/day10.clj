68a74a716f46d64ef5b5678a0b7630dc0b2d7839
alphajuliet <andrew@alphajuliet.com>
2024-01-02 20:26:53 +1100

    Park 10.2 working

--- src/aoc23/day10.clj
+++ src/aoc23/day10.clj
@@ -4,6 +4,8 @@
 (def testf "data/day10-test.txt")
 (def test2f "data/day10-test2.txt")
 (def test3f "data/day10-test3.txt")
+(def test4f "data/day10-test4.txt")
+(def test5f "data/day10-test5.txt")
 (def inputf "data/day10-input.txt")
 
 (defn find-start-node
@@ -16,8 +18,8 @@
 
 (defn navigate-loop
   "Navigate the loop and update the position based on the character and the entry direction"
-  [data rc [dr dc]]
-  (let [val (get-in data rc)
+  [grid rc [dr dc]]
+  (let [val (get-in grid rc)
         dir' (case val
                \F (if (zero? dr) [1 0] [0 1])
                \7 (if (zero? dr) [1 0] [0 -1])
@@ -37,22 +39,61 @@
     :else {:rc [(inc r) c] :dir [0 1]}))
 
 (defn traverse-loop
-  [data]
-  (let [start (first (find-start-node data))]
+  "Traverse the loop from the start position, counting the steps and all the visited positions."
+  [grid]
+  (let [start (first (find-start-node grid))]
     (set! *print-length* 5) ; help the debugger
     (loop [{:keys [rc dir]} (find-first-step start)
+           visited [start]
            length 0]
-      (let [state (navigate-loop data rc dir)]
+      (let [state (navigate-loop grid rc dir)]
         (if (= (:rc state) start)
-          (inc length)
+          {:length (inc length), :visited visited}
           ;; else
-          (recur state (inc length)))))))
+          (recur state (conj visited (:rc state)) (inc length)))))))
+
+(defn find-crossings
+  "Find where the loop crosses a row"
+  [loop-rc row maxc]
+  (let [xx (set (filter #(= row (first %)) loop-rc))]
+    (loop [posns []
+           col 0 
+           i 0]
+      (if (>= col maxc)
+        posns
+        ;; else
+        (if (and (contains? xx [row col])
+                 (odd? i))
+          (recur (conj posns [row col]) (inc col) (inc i))
+          ;; else
+          (recur posns (inc col) i)
+          )))))
+
+(defn scan-loop
+  "Scan the rows of the grid and identify all spaces inside the grid."
+  [grid loop-rc]
+  (let [maxr (count grid)
+        maxc (count (first grid))]
+        ;; Scan each row and identify any spaces that are an odd number of crossings of the loop
+(for [r (range maxr)
+      c (range maxc)
+      :let [x (find-crossings loop-rc r maxc)]
+      :when true]
+    [r c])))
 
 (defn part1
   [f]
   (let [data (util/import-data f)]
     (-> data
         traverse-loop
+        (:length)
         (quot 2))))
+
+(defn part2
+  [f]
+  (let [data (util/import-data f)]
+    (-> data
+        traverse-loop
+        (:visited))))
 
 ;; The End
